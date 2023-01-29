487360c847ec0fffca3e8a17cb1f906b9e9f8bb5
Viktoria <vikatim4encko@yandex.ru>
2023-01-29 19:59:50 +0300

    Выполнение задания по нахождению массива из строк, длина которых меньше или равно 3

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] Mas = new string[10] { "777", "Tim", "13", "mas", "world", "-2", ":)", "Russian", "computer science", "Simba" };
+string[] MasNew = new string[Mas.Length];
+void SecondMas(string[] Mas, string[] MasNew)
+{
+    int count = 0;
+    for (int i = 0; i < Mas.Length; i++)
+    {
+        if (Mas[i].Length <= 3)
+        {
+            MasNew[count] = Mas[i];
+            count++;
+        }
+    }
+}
+void PrintMas(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondMas(Mas, MasNew);
+Console.WriteLine($"Массив из строк, длина которых меньше или равно 3: ");
+PrintMas(MasNew);
